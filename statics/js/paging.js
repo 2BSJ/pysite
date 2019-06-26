2637dae88a7c53c3fe56c146ff2f7aed73f5f6da
2BSJ <h3tmdwns@gmail.com>
2019-06-26 09:33:00 +0900

    js리스트카운트 수정(search일때)

--- statics/js/paging.js
+++ statics/js/paging.js
@@ -13,26 +13,55 @@ var setPage = function(listCount, currentPage, search){
 		console.log("endPage", endPage);
 		console.log("displayPage", displayPage);
 		console.log("search",search);
-
 		var pager =$('#pager');
-		if(currentPage <= 5){
-			pager.prepend('<li>◀</li>');
-		}else{
-			pager.prepend('<li><a href=/board/list?page='+(displayPage-5)+'>◀</a></li>');
+
+        //찾기일때 로직
+		if(search){
+
+		    if(currentPage <= 5){
+                pager.prepend('<li>◀</li>');
+            }else{
+                pager.prepend('<li><a href=/board/search?search='+search+'&page='+(displayPage-5)+'>◀</a></li>');
+            }
+            for (var i = displayPage-4; i <= displayPage; i++) {
+                if(i==currentPage){
+                    pager.append('<li class="selected">'+i+'</li>')
+                    continue;
+                }else if(i>pageCount){
+                    pager.append('<li>'+i+'</li>')
+                    continue;
+                }
+                pager.append('<li><a href=/board/search?search='+search+'&page='+i+'>'+i+'</a></li>');
+            }
+            if(displayPage < endPage){
+                pager.append('<li><a href=/board/search?search='+search+'&page='+(displayPage+1)+'>▶</a></li>');
+            }else{
+                pager.append('<li>▶</li>')
+            }
 		}
-		for (var i = displayPage-4; i <= displayPage; i++) {
-			if(i==currentPage){
-				pager.append('<li class="selected">'+i+'</li>')
-				continue;
-			}else if(i>pageCount){
-				pager.append('<li>'+i+'</li>')
-				continue;
-			}
-			pager.append('<li><a href=/board/list?page='+i+'>'+i+'</a></li>');
-		}
-		if(displayPage < endPage){
-			pager.append('<li><a href=/board/list?page='+(displayPage+1)+'>▶</a></li>');
-		}else{
-			pager.append('<li>▶</li>')
+
+        //#일반 게시판 눌렀을때 로직
+		else{
+
+            if(currentPage <= 5){
+                pager.prepend('<li>◀</li>');
+            }else{
+                pager.prepend('<li><a href=/board/list?page='+(displayPage-5)+'>◀</a></li>');
+            }
+            for (var i = displayPage-4; i <= displayPage; i++) {
+                if(i==currentPage){
+                    pager.append('<li class="selected">'+i+'</li>')
+                    continue;
+                }else if(i>pageCount){
+                    pager.append('<li>'+i+'</li>')
+                    continue;
+                }
+                pager.append('<li><a href=/board/list?page='+i+'>'+i+'</a></li>');
+            }
+            if(displayPage < endPage){
+                pager.append('<li><a href=/board/list?page='+(displayPage+1)+'>▶</a></li>');
+            }else{
+                pager.append('<li>▶</li>')
+            }
 		}
 }
